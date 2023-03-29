4e54cb286079e97289d3e1e0a21e29ef95ffae65
NSTSeek <nstseek@NSTSeeks-MacBook-Pro.local>
2023-03-29 03:56:20 -0300

    feat: Creates FE integration for loaded-cost endpoint

--- src/pages/cadastrar-transporte/hooks/useCalculateTransporte.tsx
+++ src/pages/cadastrar-transporte/hooks/useCalculateTransporte.tsx
@@ -1,20 +1,78 @@
-import { useGetFreightLoadedCost } from "@/api/Freight/useGetFreightLoadedCost";
+import { useGetFreightLoadedCostBulk } from "@/api/Freight/useGetFreightLoadedCostBulk";
+import { makeCityRoute } from "./useCalculateTransporte.model";
+import { useCreateCidadesForm } from "./useCreateCidadesForm";
 import { useCreateObjetosForm } from "./useCreateObjetosForm";
 
 export const useCalculateTransporte = ({
   objetosListForm,
+  cidadesListForm,
 }: {
   objetosListForm: ReturnType<typeof useCreateObjetosForm>["objetosListForm"];
+  cidadesListForm: ReturnType<typeof useCreateCidadesForm>["cidadesListForm"];
 }) => {
-  const query = useGetFreightLoadedCost();
+  const query = useGetFreightLoadedCostBulk();
 
   const { mutate } = query;
 
+  type Body = Parameters<typeof mutate>[0][0];
+
   const submit = () => {
     const { objetos } = objetosListForm.getValues();
+    const { cidades } = cidadesListForm.getValues();
 
-    // TODO: create the logic to integrate with BE
-    // objetos[0].
+    type ObjectWithRoute = {
+      id: number;
+      quantity: number;
+      route: number[];
+    };
+
+    const objectsWithRoute: ObjectWithRoute[] = [];
+
+    objetos.forEach((objeto) => {
+      const objectWithRoute: ObjectWithRoute = {
+        id: Number(objeto.value),
+        quantity: Number(objeto.quantity),
+        route: makeCityRoute({ cidades, destinationId: objeto.cidadeDestino }),
+      };
+
+      objectsWithRoute.push(objectWithRoute);
+    });
+
+    const bodies: Body[] = [];
+
+    cidades.forEach((cidade, index) => {
+      if (index === 0) {
+        return;
+      }
+      const previousCityId = Number(cidades[index - 1].value);
+      const actualCityId = Number(cidade.value);
+
+      const objectsRoutedThroughThisCity = objectsWithRoute.filter(
+        (objectWithRoute) => objectWithRoute.route.includes(actualCityId)
+      );
+
+      console.log(objectsRoutedThroughThisCity);
+
+      const objectIdsWithQuantity: Body["objectIdsQuantity"] = {};
+
+      objectsRoutedThroughThisCity.forEach((object) => {
+        if (isNaN(objectIdsWithQuantity[object.id])) {
+          objectIdsWithQuantity[object.id] = object.quantity;
+        } else {
+          objectIdsWithQuantity[object.id] += object.quantity;
+        }
+      });
+
+      const body: Body = {
+        cidadeAId: previousCityId,
+        cidadeBId: actualCityId,
+        objectIdsQuantity: objectIdsWithQuantity,
+      };
+
+      bodies.push(body);
+    });
+
+    return mutate(bodies);
   };
 
   return { ...query, submit };
